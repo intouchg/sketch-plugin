f8677cc2148146811ed3f336bd7266d83bde8b28
Cody Persinger <cody.persinger@intouchsol.com>
2020-10-11 03:02:20 -0500

    Cleanup

--- app/components/ImportModal/index.tsx
+++ app/components/ImportModal/index.tsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import styled from 'styled-components'
-import { HashRouter, Switch, Route, Redirect } from 'react-router-dom'
 import { Flex, Box } from '@i/components'
 import { RightToolbar } from './RightToolbar'
 
@@ -15,53 +14,29 @@ const ImportModal = ({
 	closeImportModal: () => void
 }) => {
 	return (
-		<HashRouter>
+		<Flex
+			position="fixed"
+			top="0"
+			width="100vw"
+			height="100vh"
+			alignItems="center"
+			justifyContent="center"
+			backgroundColor="rgba(0, 0, 0, 0.3)"
+			zIndex={4}
+		>
 			<Flex
-				position="fixed"
-				top="0"
-				width="100vw"
-				height="100vh"
-				alignItems="center"
-				justifyContent="center"
-				backgroundColor="rgba(0, 0, 0, 0.3)"
-				zIndex={4}
+				width="calc(100vw - 308px)"
+				height="calc(100vh - 100px)"
+				backgroundColor="Card"
+				boxShadow="Medium"
+				borderRadius="Large"
 			>
-				<Flex
-					width="calc(100vw - 308px)"
-					height="calc(100vh - 100px)"
-					backgroundColor="Card"
-					boxShadow="Medium"
-					borderRadius="Large"
-				>
-					<ResponsiveContainer>
-						<Switch>
-							<Route path="/colors">
-								Colors
-							</Route>
-							<Route path="/fonts">
-								Fonts
-							</Route>
-							<Route path="/typescale">
-								Type Scale
-							</Route>
-							<Route path="/shadows">
-								Shadows
-							</Route>
-							<Route path="/borders">
-								Borders
-							</Route>
-							<Route path="/radii">
-								Radii
-							</Route>
-							<Route path="/">
-								<Redirect to="/colors" />
-							</Route>
-						</Switch>
-					</ResponsiveContainer>
-					<RightToolbar closeImportModal={closeImportModal} />
-				</Flex>
+				<ResponsiveContainer>
+					Content goes here
+				</ResponsiveContainer>
+				<RightToolbar closeImportModal={closeImportModal} />
 			</Flex>
-		</HashRouter>
+		</Flex>
 	)
 }
 
