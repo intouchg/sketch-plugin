7421f2b1927be5a868ba445cb95e4d0962ce569d
Cody Persinger <cody.persinger@intouchsol.com>
2020-10-15 02:09:27 -0500

    Cleanup store

--- app/store/index.ts
+++ app/store/index.ts
@@ -1,3 +1,12 @@
-export * from './rootReducer'
-export * from './store'
+import { combineReducers, createStore } from 'redux'
+import { themeReducer } from './theme'
+
 export * from './theme'
+
+const rootReducer = combineReducers({
+	theme: themeReducer,
+})
+
+export const store = createStore(rootReducer)
+
+export type RootState = ReturnType<typeof rootReducer>
--- app/store/rootReducer.ts
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from 'redux'
-import { themeReducer } from './theme'
-
-const rootReducer = combineReducers({
-	theme: themeReducer,
-})
-
-export default rootReducer
