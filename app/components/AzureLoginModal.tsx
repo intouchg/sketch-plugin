c27be5420dd8547373ed5411f7e684bb92dc41ec
Cody Persinger <cody.persinger@cpersinger-mac.lan1>
2020-12-16 03:55:33 -0600

    Scaffold Azure modals

--- app/components/AzureLoginModal.tsx
+++ app/components/AzureLoginModal.tsx
@@ -1,6 +1,9 @@
 import React from 'react'
-import { Flex } from '@i/components'
+import { Flex, Stack, Heading } from '@i/components'
 import { ModalBackground } from './ModalBackground'
+import { InvisibleButton, PrimaryButton } from './Buttons'
+import { CloseIcon } from './Icons'
+import { AccentText } from './Texts'
 
 const AzureLoginModal = ({
 	closeAzureLoginModal,
@@ -9,8 +12,50 @@ const AzureLoginModal = ({
 }) => {
 	return (
 		<ModalBackground>
-			<Flex>
-				Test
+			<Flex
+				width="560px"
+				height="364px"
+				padding={5}
+				backgroundColor="Background"
+				boxShadow="Medium"
+				borderRadius="Large"
+			>
+				<InvisibleButton
+					position="absolute"
+					top="0"
+					right="0"
+					padding={2}
+					zIndex={4}
+					onClick={closeAzureLoginModal}
+				>
+					<CloseIcon
+						width="13px"
+						fill="Accent"
+					/>
+				</InvisibleButton>
+				<Stack
+					flexGrow={1}
+					overflow="hidden"
+				>
+					<Heading marginBottom={4}>
+						Azure
+					</Heading>
+					<Stack marginBottom={3}>
+						<AccentText marginBottom={2}>
+							Email Address
+						</AccentText>
+					</Stack>
+					<Stack marginBottom={4}>
+						<AccentText marginBottom={2}>
+							Access Token
+						</AccentText>
+					</Stack>
+					<Flex alignSelf="start">
+						<PrimaryButton>
+							Log In
+						</PrimaryButton>
+					</Flex>
+				</Stack>
 			</Flex>
 		</ModalBackground>
 	)
--- app/components/AzureStatusModal.tsx
+++ app/components/AzureStatusModal.tsx
@@ -1,6 +1,9 @@
 import React from 'react'
-import { Flex } from '@i/components'
+import { Flex, Stack, Heading } from '@i/components'
 import { ModalBackground } from './ModalBackground'
+import { InvisibleButton, PrimaryButton } from './Buttons'
+import { CloseIcon } from './Icons'
+import { AccentText } from './Texts'
 
 const AzureStatusModal = ({
 	closeAzureStatusModal,
@@ -9,8 +12,38 @@ const AzureStatusModal = ({
 }) => {
 	return (
 		<ModalBackground>
-			<Flex>
-				Test
+			<Flex
+				width="560px"
+				height="364px"
+				padding={5}
+				backgroundColor="Background"
+				boxShadow="Medium"
+				borderRadius="Large"
+			>
+				<InvisibleButton
+					position="absolute"
+					top="0"
+					right="0"
+					padding={2}
+					zIndex={4}
+					onClick={closeAzureStatusModal}
+				>
+					<CloseIcon
+						width="13px"
+						fill="Accent"
+					/>
+				</InvisibleButton>
+				<Stack
+					flexGrow={1}
+					overflow="hidden"
+				>
+					<Heading marginBottom={4}>
+						Azure
+					</Heading>
+					<PrimaryButton>
+						Save to Azure
+					</PrimaryButton>
+				</Stack>
 			</Flex>
 		</ModalBackground>
 	)
