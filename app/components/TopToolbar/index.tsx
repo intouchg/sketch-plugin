9b496ee7c4ccb2305448d52d06962a0804c71967
Cody Persinger <cody.persinger@cpersinger-mac.lan1>
2021-01-04 14:51:25 -0600

    Add localProject name to TopToolbar when available

--- app/components/TopToolbar/index.tsx
+++ app/components/TopToolbar/index.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { useSelector } from 'react-redux'
 import { Flex, Heading, Box } from '@i/components'
 import { ImportButton } from './ImportButton'
 import { PreviewButton } from './PreviewButton'
@@ -12,29 +13,33 @@ const TopToolbar = ({
 }: {
 	setShowImportModal?: React.Dispatch<React.SetStateAction<boolean>>
 	setShowAzureModal: React.Dispatch<React.SetStateAction<boolean>>
-}) => (
-	<Flex
-		height={topToolbarHeight}
-		alignItems="center"
-		justifyContent="space-between"
-		backgroundColor="Card"
-		paddingX={3}
-		boxShadow="Medium"
-		zIndex={3}
-	>
-		<Heading>
-			IDS Plugin
-		</Heading>
-		<Box>
-			{setShowImportModal && (
-				<>
-					<ImportButton setShowImportModal={setShowImportModal} />
-					<PreviewButton />
-				</>
-			)}
-			<AzureStatus setShowAzureModal={setShowAzureModal} />
-		</Box>
-	</Flex>
-)
+}) => {
+	const localProject = useSelector((state) => state.azure.localProject)
+
+	return (
+		<Flex
+			height={topToolbarHeight}
+			alignItems="center"
+			justifyContent="space-between"
+			backgroundColor="Card"
+			paddingX={3}
+			boxShadow="Medium"
+			zIndex={3}
+		>
+			<Heading>
+				{localProject ? localProject.split('/').pop() : 'IDS Plugin'}
+			</Heading>
+			<Box>
+				{setShowImportModal && (
+					<>
+						<ImportButton setShowImportModal={setShowImportModal} />
+						<PreviewButton />
+					</>
+				)}
+				<AzureStatus setShowAzureModal={setShowAzureModal} />
+			</Box>
+		</Flex>
+	)
+}
 
 export { TopToolbar }
