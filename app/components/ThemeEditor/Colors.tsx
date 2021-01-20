5235493618214ee5eb177155515f6dc4b7eaf73f
Cody Persinger <cody.persinger@cpersinger-mac.lan1>
2021-01-20 02:18:27 -0600

    Fix scrolling on ThemeEditor/Colors

--- app/components/ThemeEditor/Colors.tsx
+++ app/components/ThemeEditor/Colors.tsx
@@ -4,34 +4,41 @@ import { Box } from '@i/components'
 import { Color } from '../ThemeValues'
 import { InvisibleButton } from '../Buttons'
 import { ColorGrid } from '../ColorGrid'
+import { topToolbarHeight } from '../TopToolbar'
 
 const Colors = () => {
 	const colors = useSelector((state) => state.theme.values.colors)
 
 	return (
-		<ColorGrid
-			maxWidth="860px"
-			marginX="auto"
-			gridGap={3}
-			padding={6}
+		<Box
+			width="100%"
+			height={`calc(100vh - ${topToolbarHeight})`}
+			overflow="scroll"
 		>
-			{colors.map((color) => (
-				<Box
-					key={color.id}
-					position="relative"
-					height="0"
-					paddingBottom="65.31%"
-					flexGrow={1}
-					as={InvisibleButton}
-					onClick={() => {}}
-				>
-					<Color
-						selected={false}
-						{...color}
-					/>
-				</Box>
-			))}
-		</ColorGrid>
+			<ColorGrid
+				maxWidth="860px"
+				marginX="auto"
+				gridGap={3}
+				padding={6}
+			>
+				{colors.map((color) => (
+					<Box
+						key={color.id}
+						position="relative"
+						height="0"
+						paddingBottom="65.31%"
+						flexGrow={1}
+						as={InvisibleButton}
+						onClick={() => {}}
+					>
+						<Color
+							selected={false}
+							{...color}
+						/>
+					</Box>
+				))}
+			</ColorGrid>
+		</Box>
 	)
 }
 
