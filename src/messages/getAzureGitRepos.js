5b52f535c7901be58c4b73e68b193429094d4c53
Cody Persinger <cody.persinger@cpersinger-mac.lan1>
2021-01-24 00:22:21 -0600

    Update getAzureGitRepos to reset credentials data when failing to login

--- src/messages/getAzureGitRepos.js
+++ src/messages/getAzureGitRepos.js
@@ -1,9 +1,17 @@
-import { getAzureGitRepos as get, connectToAzure, readAzureCredentialsMetadata } from '../services'
+import { getAzureGitRepos as get, connectToAzure, readAzureCredentialsMetadata, writeAzureCredentialsMetadata } from '../services'
 
 export const getAzureGitRepos = async (state, payload) => {
 	try {
 		const credentials = readAzureCredentialsMetadata()
-		await connectToAzure(credentials.username, credentials.accessToken)
+
+		try {
+			await connectToAzure(credentials.username, credentials.accessToken)
+		}
+		catch (loginError) {
+			writeAzureCredentialsMetadata({ username: credentials.username, accessToken: '' })
+			throw Error('Failed to authenticate with Azure: ' + loginError)
+		}
+
 		const gitRepos = await get()
 		return gitRepos
 	}
